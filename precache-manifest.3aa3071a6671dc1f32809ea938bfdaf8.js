4cd4b4aa4b617059f3aaf512fa3f58328e3843a9
git-ben18 <zook.6@wright.edu>
2020-09-06 19:06:29 -0400

    Updates

---
+++ precache-manifest.3aa3071a6671dc1f32809ea938bfdaf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab70a760fa4f31e0f52a4b5ee1ab4716",
+    "url": "/mining-edm/index.html"
+  },
+  {
+    "revision": "d2e5997df62154e132dc",
+    "url": "/mining-edm/static/css/main.606b47fd.chunk.css"
+  },
+  {
+    "revision": "904f60c24b3803cd6482",
+    "url": "/mining-edm/static/js/2.7bffe5c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mining-edm/static/js/2.7bffe5c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e5997df62154e132dc",
+    "url": "/mining-edm/static/js/main.80122600.chunk.js"
+  },
+  {
+    "revision": "323885d0b107ab80ede5",
+    "url": "/mining-edm/static/js/runtime-main.15644427.js"
+  }
+]);
